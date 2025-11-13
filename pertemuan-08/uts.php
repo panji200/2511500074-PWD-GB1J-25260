3f6a091d42e4817f30ba9a1565ca96b7a32d7dc4
Panji <2511500074@mahasiswa.atmaluhur.ac.id>
2025-11-13 18:44:08 +0700

    about ditentang saya lakukan echo sesuai nama fariabel yang telah kita buat

--- pertemuan-08/uts.php
+++ pertemuan-08/uts.php
@@ -28,21 +28,3 @@
     // Redirect kembali ke index.php
 header('Location: index.php');
 exit();
-?>
-    <?php
-session_start();
-
-// Terima data dari form
-$nama = $_POST['txtNama'] ?? '';
-$email = $_POST['txtEmail'] ?? '';
-$pesan = $_POST['txtPesan'] ?? '';
-
-// Simpan ke session
-$_SESSION['sesnama'] = $nama;
-$_SESSION['sesemail'] = $email;
-$_SESSION['sespesan'] = $pesan;
-
-// Redirect kembali ke index.php
-header('Location: index.php');
-exit();
-?>
