09900dcd81b65603e410d6089688f3eb5e4cb5a1
Panji <2511500074@mahasiswa.atmaluhur.ac.id>
2026-01-09 06:43:45 +0700

    membuat table baru mahasiswa

--- pertemuan-13/README.md
+++ pertemuan-13/README.md
@@ -1,3 +1,1 @@
-# Tugas pertemuan-13
-
-Repository untuk tugas pertemuan-13 mata kuliah Pemrograman Web Dasar
+# pertemuan-15
---
+++ pertemuan-13/mahasiswa.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 08, 2026 at 11:40 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pwd2025`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(15) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `alamat` text,
+  `jurusan` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `alamat`, `jurusan`, `created_at`) VALUES
+('2411500023', 'ERI ARDIANSAH SAPUTRA', 'SELINDUNG', 'TI T', '2026-01-08 22:44:45'),
+('2511500078', 'panjiiii', 'beruas', 'TI teknik informatika', '2026-01-08 21:23:21');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
