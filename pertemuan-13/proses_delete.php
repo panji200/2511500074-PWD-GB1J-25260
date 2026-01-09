0eae1e00642e5b0a3f2147087c98dbfc74a0dab7
Panji <2511500074@mahasiswa.atmaluhur.ac.id>
2026-01-09 07:48:26 +0700

    membuat proses_delete.php untuk data mahasiswa

--- pertemuan-13/proses_delete.php
+++ pertemuan-13/proses_delete.php
@@ -3,40 +3,39 @@
   require __DIR__ . '/koneksi.php';
   require_once __DIR__ . '/fungsi.php';
 
-  #validasi cid wajib angka dan > 0
-  $cid = filter_input(INPUT_GET, 'cid', FILTER_VALIDATE_INT, [
-    'options' => ['min_range' => 1]
-  ]);
+  #validasi nim wajib ada
+  $nim = $_GET['nim'] ?? '';
 
-  if (!$cid) {
-    $_SESSION['flash_error'] = 'CID Tidak Valid.';
+  if ($nim === '') {
+    $_SESSION['flash_error'] = 'NIM Tidak Valid.';
     redirect_ke('read.php');
   }
 
   /*
     Prepared statement untuk anti SQL injection.
-    menyiapkan query UPDATE dengan prepared statement 
-    (WAJIB WHERE cid = ?)
+    menyiapkan query DELETE dengan prepared statement 
+    (WAJIB WHERE nim = ?)
   */
-  $stmt = mysqli_prepare($conn, "DELETE FROM tbl_tamu
-                                WHERE cid = ?");
+  $stmt = mysqli_prepare($conn, "DELETE FROM mahasiswa
+                                WHERE nim = ?");
   if (!$stmt) {
     #jika gagal prepare, kirim pesan error (tanpa detail sensitif)
     $_SESSION['flash_error'] = 'Terjadi kesalahan sistem (prepare gagal).';
     redirect_ke('read.php');
   }
 
-  #bind parameter dan eksekusi (s = string, i = integer)
-  mysqli_stmt_bind_param($stmt, "i", $cid);
+  #bind parameter dan eksekusi (s = string)
+  mysqli_stmt_bind_param($stmt, "s", $nim);
 
-  if (mysqli_stmt_execute($stmt)) { #jika berhasil, kosongkan old value
+  if (mysqli_stmt_execute($stmt)) { #jika berhasil
     /*
       Redirect balik ke read.php dan tampilkan info sukses.
     */
-    $_SESSION['flash_sukses'] = 'Terima kasih, data Anda sudah dihapus.';
-  } else { #jika gagal, simpan kembali old value dan tampilkan error umum
+    $_SESSION['flash_sukses'] = 'Data mahasiswa berhasil dihapus.';
+  } else { #jika gagal
     $_SESSION['flash_error'] = 'Data gagal dihapus. Silakan coba lagi.';
   }
+
   #tutup statement
   mysqli_stmt_close($stmt);
 
